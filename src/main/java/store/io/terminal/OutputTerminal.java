571b50345c669c2290487dc50c981af0dc99e05e
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2024-11-14 03:00:46 +0900

    refactor: 영수증 formatting 로직화

--- src/main/java/store/io/terminal/OutputTerminal.java
+++ src/main/java/store/io/terminal/OutputTerminal.java
@@ -1,5 +1,10 @@
 package store.io.terminal;
 
+import static store.io.terminal.ReceiptLineFormatter.FormatType.AMOUNT;
+import static store.io.terminal.ReceiptLineFormatter.FormatType.PAYMENT;
+import static store.io.terminal.ReceiptLineFormatter.formattedAmount;
+import static store.io.terminal.ReceiptLineFormatter.formattedTotalAmount;
+
 import java.util.List;
 import store.view.AmountPartView;
 import store.view.ProductPartView;
@@ -37,7 +42,10 @@ public class OutputTerminal {
 
     public void printReceipt(final ReceiptView view) {
         Writer.simplePrint("==============W 편의점================");
-        Writer.simplePrintf("%-18s %-7s %-7s", "상품명", "수량", "금액");
+        Writer.simplePrint(
+                ReceiptLineFormatter.fixedFormat("상품명", "수량", "금액")
+        );
+
         ProductPartView productPart = view.getProductPart();
         printProductPartOf(productPart);
 
@@ -47,42 +55,39 @@ public class OutputTerminal {
 
     private void printProductPartOf(final ProductPartView productPartView) {
         List<PurchasedView> purchasedViews = productPartView.getPurchasedViews();
-        purchasedViews.forEach(view -> {
-            String format = FormatProperty.getFormat(view.getProductName(), view.getTotalPrice());
-            String message = String.format(format, view.getProductName(), view.getQuantity(), view.getTotalPrice());
-            Writer.simplePrint(message);
-        });
+        purchasedViews.forEach(view ->
+                Writer.simplePrint(
+                        formattedTotalAmount(view.getProductName(), view.getQuantity(), view.getTotalPrice(), AMOUNT)
+                ));
 
         Writer.simplePrint("=============증       정==============");
+
         List<PromotionProductView> promotionProductViews = productPartView.getPromotionProductViews();
-        promotionProductViews.forEach(view -> {
-            String format = FormatProperty.leftLineFormat(view.getProductName());
-            String message = String.format(format + " %-8s",
-                    view.getProductName(), view.getQuantity(), "");
-            Writer.simplePrint(message);
-        });
+        promotionProductViews.forEach(view ->
+                Writer.simplePrint(
+                        formattedTotalAmount(view.getProductName(), view.getQuantity(), "", AMOUNT)
+                )
+        );
     }
 
     private void printAmountPartOf(final AmountPartView amountPartView) {
         Writer.simplePrint("======================================");
-        String totalPurchasedAmountFormatter = FormatProperty.getFormat("총구매액", amountPartView.getTotalAmount());
-        String totalPurchasedAmount = String.format(totalPurchasedAmountFormatter,
-                "총구매액", amountPartView.getTotalQuantity(), amountPartView.getTotalAmount());
-        Writer.simplePrint(totalPurchasedAmount);
-
-        String promotionDiscountFormatter = FormatProperty.getFormat("행사할인", amountPartView.getPromotionDiscount());
-        String promotionDiscount = String.format(promotionDiscountFormatter,
-                "행사할인", "", amountPartView.getPromotionDiscount());
-        Writer.simplePrint(promotionDiscount);
-
-        String membershipDiscountFormatter = FormatProperty.getFormat("멤버십할인", amountPartView.getMembershipDiscount());
-        String membershipDiscount = String.format(membershipDiscountFormatter,
-                "멤버십할인", "", amountPartView.getMembershipDiscount());
-        Writer.simplePrint(membershipDiscount);
-
-        String paymentFormatter = FormatProperty.paymentFormat("내실돈", amountPartView.getPayment());
-        String payment = String.format(paymentFormatter,
-                "내실돈", "", amountPartView.getPayment());
-        Writer.simplePrint(payment);
+        Writer.simplePrint(
+                formattedTotalAmount(
+                        "총구매액",
+                        amountPartView.getTotalQuantity(),
+                        amountPartView.getTotalAmount(),
+                        AMOUNT
+                )
+        );
+        Writer.simplePrint(
+                formattedAmount("행사할인", amountPartView.getPromotionDiscount(), AMOUNT)
+        );
+        Writer.simplePrint(
+                formattedAmount("멤버십할인", amountPartView.getMembershipDiscount(), AMOUNT)
+        );
+        Writer.simplePrint(
+                formattedAmount("내실돈", amountPartView.getPayment(), PAYMENT)
+        );
     }
 }
